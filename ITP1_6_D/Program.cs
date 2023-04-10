a3c8a5aa1b4f09825db815102009770ddd0ccb2c
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:07:46 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_6_D/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ITP1_6_D
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] Input = Console.ReadLine().Split(' ');
+            var n = int.Parse(Input[0]);
+            var m = int.Parse(Input[1]);
+
+            int[,] A = new int[n, m];
+            int[] b = new int[m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; m++)
+                {
+                    A[i, j] = int.Parse(Console.ReadLine());
+                } 
+            }
+            for (int k = 0; k < m; k++)
+            {
+                b[k] = int.Parse(Console.ReadLine());
+            }
+
+            for(int l = 0; l < n; l++)
+            {
+
+                for(int p = 0; p < m; p++)
+                {
+
+                }
+            }
+        }
+    }
+}
